9ba5100fb55ae81c912a6d56ad37bc682ac871ff
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-19 00:29:04 -0300

    :chore: Aula 10 - Interfaces

---
+++ 10 - Interfaces/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _10___Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IEletroDomestico lampada = new Lampada();
+            lampada.ligar();
+
+            IPotencia lampada2 = new Lampada();
+            lampada2.whats = 13.3;
+
+            Console.WriteLine(lampada2.ToString());
+            
+        }
+    }
+
+    interface IPotencia 
+    {
+        double whats { get; set; }
+    }
+    interface IEletroDomestico
+    {
+        void ligar()
+        {
+            Console.WriteLine("LIGOU!");
+        }
+
+        void desligar()
+        {
+
+        }
+    }
+
+    class Lampada : IEletroDomestico, IPotencia
+    {
+        public double whats { get; set; }
+
+        void ligar()
+        {
+
+        }
+
+        void desligar()
+        {
+
+        }
+    }
+
+
+    class Televisao : IEletroDomestico
+    {
+        void ligar()
+        {
+
+        }
+
+        void desligar()
+        {
+
+        }
+    }
+}
