ad858133ac0d4dd4a06676d7b4c2624ccf58430a
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-18 23:14:37 -0300

    :chore: Aula tipos de referências.

---
+++ 8 - Tipos de Referência/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _8___Tipos_de_Referência
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cliente1 = new Cliente("Vinicius", 22);
+            var cliente2 = cliente1;
+            Console.WriteLine($"Cliente1: {cliente1}");
+            Console.WriteLine($"Cliente1: {cliente2}");
+
+            cliente1.Nome = "HEAD";
+
+            Console.WriteLine($"Cliente1: {cliente1}");
+            Console.WriteLine($"Cliente1: {cliente2}");
+        }
+    } 
+
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Cliente(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Idade: {Idade}";
+        }
+    }
+}
