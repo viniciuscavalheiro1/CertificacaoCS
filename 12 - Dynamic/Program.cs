1cce41227ecb0799a5c622e9f0d1d6246381624d
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-23 20:32:57 -0300

    Aula 12 - Tipos Dinamicos

---
+++ 12 - Dynamic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _12___Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object objeto = 1;
+
+            dynamic dinamico = 1;
+
+            dinamico = dinamico + 3;
+
+            Console.WriteLine(dinamico);
+        }
+    }
+}
