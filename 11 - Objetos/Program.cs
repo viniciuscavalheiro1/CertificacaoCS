367acc4a6fb75560db2b7d20ee753372c7430185
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-22 23:30:01 -0300

    :chore: Aula 11 - Objetos

---
+++ 11 - Objetos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11___Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object meuObjeto;
+
+            meuObjeto = 10;
+
+            meuObjeto = new Jogador();
+            Console.WriteLine(meuObjeto);
+            Jogador classRef;
+
+            classRef = (Jogador)meuObjeto;
+        }
+    }
+
+    class Jogador
+    {
+        public int Pontuacao { get; set; } = 10;
+    }
+}
