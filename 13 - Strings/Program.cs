adda4104633be2271e8d35a450b85acc9f7d0088
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-23 21:02:04 -0300

    :chore: Aula 13 - Strings

---
+++ 13 - Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _13___Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Bom dia";
+            string b = "B";
+
+            b = b + "om dia";
+
+            Console.WriteLine(a);
+            Console.WriteLine(a == b);
+            Console.WriteLine((object)a == (object)b);
+
+            string bomDia = "Bom dia";
+
+            Console.WriteLine($"Bom dia {bomDia[4]}");
+        }
+    }
+}
