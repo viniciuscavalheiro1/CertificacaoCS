8a50785b6156bf1f941c906cf1c4aff2e59e75c4
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-26 20:41:13 -0300

    :chore: Argumentos definidos

--- 15 - Argumentos Nomeados/Program.cs
+++ 15 - Argumentos Nomeados/Program.cs
@@ -7,11 +7,39 @@ namespace _15___Argumentos_Nomeados
         static void Main(string[] args)
         {
             Console.WriteLine(NomeIdade(nome: "Vinícius", idade: 22));
+
+            Carro carro = new Carro();
+
+            Console.WriteLine(carro.ToString());
         }
 
         static string NomeIdade(int idade, string nome)
         {
             return $"Nome: {nome}, Idade: {idade}";
+        }
+    }
+
+    class Carro
+    {
+        string nome;
+        int ano;
+
+        public Carro(string nome, int ano)
+        {
+            this.nome = nome;
+            this.ano = ano;
+        }
+
+        public Carro(string nome = "Fiat")
+        {
+            this.nome = nome;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Nome: {nome}";
         }
     }
 
