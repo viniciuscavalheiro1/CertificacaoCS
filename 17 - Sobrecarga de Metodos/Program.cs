fa1c0c62dd7bc40cb88f8f69e4bc5e4b382e080b
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-26 22:58:37 -0300

    :chore: Aula 17 - Sobrecargas de Metodos

---
+++ 17 - Sobrecarga de Metodos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _17___Sobrecarga_de_Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        double Volume(double lado)
+        {
+            return Math.Pow(lado, 3);
+        }
+
+        double Volume(double altura, double raio)
+        {
+            return altura * Math.PI * Math.Pow(raio, 2);
+        }
+
+        double volume(double largura, double profundidade, double altura)
+        {
+            return largura * profundidade * altura;
+        }
+
+    }
+}
