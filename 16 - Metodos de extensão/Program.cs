9f296a2cd6a5231c1081f8d0b71dd3ffd53eff74
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-26 21:42:03 -0300

    :chore: Aula 16 - Metodos de Extensão

---
+++ 16 - Metodos de extensão/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _16___Metodos_de_extensão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Impressora impressora = new Impressora("HELOOO");
+            impressora.Imprimir();
+            impressora.ImprimirDocumentoHTML();
+
+        }
+    }
+
+    class Impressora
+    {
+        public string Documento { get; }
+
+        public Impressora(string documento)
+        {
+            this.Documento = documento;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Imprimindo...");
+            Console.WriteLine(Documento);
+        }
+    }
+
+    static class ImpressoraExtensions
+    {
+        public static void ImprimirDocumentoHTML(this Impressora impressora)
+        {
+            Console.WriteLine($"<html><body>{impressora.Documento}</body></html>");
+        }
+    }
+}
