78dc94d75b9c99d6c02d513359f6994d6fdf53e8
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-09-25 03:53:33 -0300

    :chore: Aula de Metodos

---
+++ Metodos/Metodos/01-Ref.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos.Metodos
+{
+    class _01_Ref
+    {
+        static void Inverter(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void Inverter()
+        {
+            int i = 1, j = 2;
+            Inverter(ref i, ref j);
+            System.Console.WriteLine($"{i} {j}");
+        }
+    }
+
+}
